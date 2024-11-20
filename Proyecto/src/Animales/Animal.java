6ab7b47e8943113cc313359a6effe7c220d25c08
Miguel <miguelangelpatracalagunes@gmail.com>
2024-11-19 19:10:01 -0600

    Proyecto_Animales

---
+++ Proyecto/src/Animales/Animal.java
@@ -0,0 +1,8 @@
+package animales;
+public abstract class Animal {
+    
+    public abstract String getAlimentos();
+    public abstract String getHabitad();
+    public abstract String getSonoido();
+    public abstract String getNombreCientifico();
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Proyectos_EE_POO
 Repositorio para las actividades/proyectos realizados en la EE Diseño y Programación Orientada a Objeto
+
+Miguel Angel Patraca Lagunes 
+
+Universidad Veracruzana 
+Semestre 2024/2025
