052d19e7cbffff81a55993790080a58577ea4daa
Miguel <miguelangelpatracalagunes@gmail.com>
2024-11-24 21:22:07 -0600

    Proyecto Composición

--- SistemaPeaje/src/App.java
+++ SistemaPeaje/src/App.java
@@ -1,23 +1,13 @@
 public class App {
     public static void main(String[] args) throws Exception {
         Peaje peaje = new Peaje("Coatza1","Centro");
+        Camion camion = new Camion("PKI-889", 2);
+        peaje.agregarVehiculo(camion);
+        Moto moto = new Moto("PKI-829");
+        peaje.agregarVehiculo(moto);
+        Carro carro = new Carro("PLI-899");
+        peaje.agregarVehiculo(carro);
         peaje.imprimir();
-        Peaje peaje2 = new Peaje("Coatza2","Centro");
-        peaje2.imprimir();
-        Peaje peaje3 = new Peaje("Coatza3","Centro");
-        peaje3.imprimir();
-        Peaje peaje4 = new Peaje("Coatza4","Centro");
-        peaje4.imprimir();
-        Peaje peaje5 = new Peaje("Coatza5","Centro");
-        peaje5.imprimir();
-        Peaje peaje6 = new Peaje("Coatza6","Centro");
-        peaje6.imprimir();
-        Peaje peaje7 = new Peaje("Coatza7","Centro");
-        peaje7.imprimir();
-        Peaje peaje8 = new Peaje("Coatza8","Centro");
-        peaje8.imprimir();
-        Peaje peaje9 = new Peaje("Coatza9","Centro");
-        peaje9.imprimir();
-        Peaje peaje10 = new Peaje("Coatza10
+        System.out.println("Total de peaje: " + peaje.calcularTotalPeaje(camion));
     }
 }
